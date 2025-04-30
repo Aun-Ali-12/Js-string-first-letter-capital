05db78b3dbeefc77fe36bd30c3953fa22b728eb4
Aun-Ali-12 <aunali29331@gmail.com>
2025-04-30 22:27:41 +0500

    first letter capitilization in string

---
+++ app.js
@@ -0,0 +1,10 @@
+let str = prompt("hello! my name is aun").toLocaleLowerCase();
+let splitString = str.split(" ");
+let finalLine = [];
+for (let i = 0; i < splitString.length; i++) {
+  let firstLetter = splitString[i].slice(0, 1).toLocaleUpperCase();
+  let secLetter = splitString[i].slice(1);
+  let concatName = firstLetter + secLetter;
+  finalLine.push(concatName);
+}
+console.log(finalLine.join(" "));
